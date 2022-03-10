0b7bb136bbd83d7dce6cd8737ed7ae97f989b2cc
Zackin5 <zachin55@gmail.com>
2022-03-09 21:14:53 -0600

    Item store function naming refactor

--- ps2.asm
+++ ps2.asm
@@ -18908,14 +18908,14 @@ loc_BD92:
 	bra.w	ProcessStealItem
 ; ------------------------------------------
 ItemStoreEventIndex:
-	bra.w	loc_BDEC
-	bra.w	loc_BE12
+	bra.w	ItemStore_Enter
+	bra.w	ItemStore_Buy_Sell_Prompt
 	bra.w	loc_BE1E
 	bra.w	loc_BE3C
 	bra.w	loc_BE4E
 	bra.w	loc_BE8A
 	bra.w	loc_BEF6
-	bra.w	loc_BF02
+	bra.w	ItemStore_Confirm_Purchase
 	bra.w	loc_BF44
 	bra.w	loc_BF56
 	bra.w	loc_BF6E
@@ -18925,26 +18925,26 @@ ItemStoreEventIndex:
 	bra.w	loc_C014
 	bra.w	loc_C026
 ; ------------------------------------------
-loc_BDEC:
+ItemStore_Enter:
 	move.l	#((WinID_StoreMeseta<<$10)|WinID_ScriptMessage2), (Window_queue).w
 	bsr.w	CheckPlanetAndCaps
-	beq.s	loc_BE06
+	beq.s	ItemStore_Greeting
 	move.w	#$810, (Script_queue).w
 	addq.w	#2, (Window_routine_3).w
 	rts
 
-loc_BE06:
-	move.w	#$801, (Script_queue).w
+ItemStore_Greeting:
+	move.w	#$801, (Script_queue).w		; Welcome. how may I be of service?
 	addq.w	#1, (Window_routine_2).w
 	rts
-loc_BE12:
+ItemStore_Buy_Sell_Prompt:
 	move.w	#WinID_BuySell, (Window_queue).w
 	addq.w	#1, (Window_routine_2).w
 	rts
 loc_BE1E:
 	move.w	($FFFFDEB6).w, d0
 	beq.s	loc_BE30
-	move.w	#$802, (Script_queue).w
+	move.w	#$802, (Script_queue).w		; Whose belongings would you like to sell?
 	addq.w	#1, (Window_routine_2).w
 	rts
 
@@ -18955,7 +18955,7 @@ loc_BE30:
 
 loc_BE3C:
 	move.w	#WinID_StoreCharList, (Window_queue).w
-	move.w	#$804, (Script_queue).w
+	move.w	#$804, (Script_queue).w		; Which do you want to sell?
 	addq.w	#1, (Window_routine_2).w
 	rts
 loc_BE4E:
@@ -18967,7 +18967,7 @@ loc_BE4E:
 	adda.w	d1, a0
 	tst.b	(a0)
 	bne.s	loc_BE7E
-	move.w	#$80F, (Script_queue).w
+	move.w	#$80F, (Script_queue).w ; $BB, " isn't carrying anything. Is there anyone else who wants to sell something?
 	addq.w	#1, (Window_routine_4).w
 	rts
 loc_BE70:
@@ -18996,7 +18996,7 @@ loc_BEBA:
 	addq.w	#1, (Window_routine_4).w
 	rts
 loc_BEC0:
-	move.w	#$803, (Script_queue).w
+	move.w	#$803, (Script_queue).w	; Let's say ", $C0, " meseta. How about it?
 	moveq	#0, d0
 	move.w	-(a0), d0
 	lsr.w	#1, d0
@@ -19019,7 +19019,7 @@ loc_BEF6:
 	addq.w	#1, (Window_routine_2).w
 	rts
 
-loc_BF02:
+ItemStore_Confirm_Purchase:
 	move.w	(Inventory_offset).w, d0
 	lsr.w	#3, d0
 	andi.w	#1, d0
@@ -19027,16 +19027,16 @@ loc_BF02:
 	move.w	d0, (Window_queue).w
 	addq.w	#1, (Window_routine_2).w
 	move.w	(Yes_no_input).w, d0
-	beq.s	loc_BF26					; if you chose YES, branch
+	beq.s	ItemStore_Complete_Purchase	; if you chose YES, branch
 	move.w	#$80B, (Script_queue).w		; "I see. That's too bad. Can I help you with anything else?"
 	rts
 
-loc_BF26:
+ItemStore_Complete_Purchase:
 	bsr.w	loc_AE2E
 	bsr.w	RemoveItemFromInventory
 	move.l	(Meseta_value).w, d0
 	bsr.w	AddToCurrentMoney
-	move.w	#$80C, (Script_queue).w
+	move.w	#$80C, (Script_queue).w ; Thank you. Is there anything else?
 	move.w	#((6<<8)|WinID_StoreMeseta), (Window_queue+2).w
 	rts
 loc_BF44:
@@ -19060,7 +19060,7 @@ loc_BF6E:
 	addq.w	#1, (Window_routine_2).w
 	rts
 loc_BF7A:
-	move.w	#$808, (Script_queue).w
+	move.w	#$808, (Script_queue).w	; Who is going to carry this item?
 	addq.w	#1, (Window_routine_2).w
 	rts
 loc_BF86:
@@ -19076,7 +19076,7 @@ loc_BF92:
 	adda.w	d1, a0
 	cmpi.b	#$10, (a0)
 	bne.s	loc_BFB6
-	move.w	#$809, (Script_queue).w
+	move.w	#$809, (Script_queue).w	; You have too much already. Can someone else take it?
 	addq.w	#1, (Window_routine_4).w
 	rts
 loc_BFB6:
@@ -19091,7 +19091,7 @@ loc_BFB6:
 	move.l	d0, (Meseta_value).w
 	bsr.w	CheckSubtractMoney
 	beq.s	loc_BFE4
-	move.w	#$807, (Script_queue).w
+	move.w	#$807, (Script_queue).w ; You don't have enough money.
 	addq.w	#1, (Window_routine_3).w
 	rts
 
@@ -19100,7 +19100,7 @@ loc_BFE4:
 	move.w	(Character_index).w, d1
 	bsr.w	AddItemToInventory2
 	move.b	#SFXID_ItemReceived, (Sound_queue).w
-	move.w	#$80A, (Script_queue).w
+	move.w	#$80A, (Script_queue).w	; Thank you. Will there be anything else?
 	addq.w	#1, (Window_routine_2).w
 	rts
 loc_C004:
